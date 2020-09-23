ea8b154f0971cd0289db93ea83add4bc16c9a7f2
Grace Kalu <gracekalu@205-185-99-20.directcom.com>
2020-09-23 16:30:38 -0600

    added stylesheet.css

--- web/home.js
+++ web/home.js
@@ -3,3 +3,15 @@ function myFunction() {
 	document.getElementById("demo").innerHTML = "Thanks for creating a shop with us. Let's serve and help you create revenue.";
 
 }
+
+function colorChange() {
+	// body...
+	var textbox_id = "colorText";
+	var txtbox = document.getElementById(textbox_id);
+
+	var div_id = "intialdiv";
+	var div = document.getElementById(div_id);
+
+	var color = txtbox.value;
+	div.style.backgoundColor = color;
+}
