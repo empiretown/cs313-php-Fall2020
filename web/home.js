3184b60dfadde3d86971cb5b3a3038744e8d7ee7
Grace Kalu <gracekalu@205-185-99-20.directcom.com>
2020-09-22 23:19:24 -0600

    added shedmarket page

--- web/home.js
+++ web/home.js
@@ -1,5 +1,5 @@
 function thefunc() {
 
-	document.getElementsById('dem').innerHTML = "Thanks for creating a shop with us. Let make serve and help you create revenue."
+	document.getElementById("demo").innerHTML = "Thanks for creating a shop with us. Let make serve and help you create revenue.";
 
 }
