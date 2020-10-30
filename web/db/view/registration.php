81df2af31a4ccd6957b4b7bd0c0d71b42e181983
gracekalu <empiretown2016@gmail.com>
2020-10-30 00:23:30 -0600

    added

--- web/db/view/registration.php
+++ web/db/view/registration.php
@@ -35,22 +35,22 @@
 ​
             <div id="form">
 ​
-            <?php
+<?php
           
-            if (isset($_SESSION['message'])) {
-                echo $_SESSION['message'];
-            }
-            ?>
+          if (isset($_SESSION['message'])) {
+              echo $_SESSION['message'];
+          }
+          ?>
 ​
-            <form action="accounts/index.php" method="post">
-                    <label for="username">User Name: </label><input type="text" name="clientUsername" id="uname" required <?php if(isset($clientUsername)){echo "value='$clientUsername'";}  ?>><br>
-                    <span>Passwords must be at least 8 characters and contain at least 1 number, 1 capital letter and 1 special character</span> 
-                    <label for="password">Password: </label>
-                    <input type="password" name="clientPassword" id="password" required pattern="(?=^.{8,}$)(?=.*\d)(?=.*\W+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"><br>
-                <input type="submit" name="submit" id="regbtn" value="Register">
+          <form action="accounts/index.php" method="post">
+                  <label for="username">User Name: </label><input type="text" name="clientUsername" id="uname" required <?php if(isset($clientUsername)){echo "value='$clientUsername'";}  ?>><br>
+                  <span>Passwords must be at least 8 characters and contain at least 1 number, 1 capital letter and 1 special character</span> 
+                  <label for="password">Password: </label>
+                  <input type="password" name="clientPassword" id="password" required pattern="(?=^.{8,}$)(?=.*\d)(?=.*\W+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"><br>
+              <input type="submit" name="submit" id="regbtn" value="Register">
 ​
-                <input type="hidden" name="action" value="registration">
-            </form>
+              <input type="hidden" name="action" value="registration">
+          </form
             </div>
         </main>
     </div>
