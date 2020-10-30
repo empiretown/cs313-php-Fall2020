ee486b1d0fc27c03e28994ffc0262235fd5314ef
gracekalu <empiretown2016@gmail.com>
2020-10-29 19:56:26 -0600

    added

--- web/db/account/index.php
+++ web/db/account/index.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
 
-  require ('../connect-db.php');
- require_once '../functions.php';
+require ('../connect-db.php');
+require_once '../functions.php';
 
  require_once '../model/product-model.php';
-// require_once '../model/account.php';
- //require_once '../model/product.php';
+ require_once '../model/account.php';
+ require_once '../model/product.php';
 
  $action = filter_input(INPUT_POST, 'action');
  if ($action == NULL) {
