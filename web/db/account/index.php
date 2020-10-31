70a539156e99e580c90b4a0f73658f21cd0d63b2
gracekalu <empiretown2016@gmail.com>
2020-10-31 17:50:39 -0600

    added

--- web/db/account/index.php
+++ web/db/account/index.php
@@ -152,7 +152,7 @@ require_once '../functions.php';
          $_SESSION['clientData'] = $clientData;
          
  // Send them to the admin view
-         include '../view/admin.php';
+         include '../view/category.php';
          exit;
          break;
  
