620c20a08511dc88c9a0dab277787c7b446890f4
gracekalu <empiretown2016@gmail.com>
2020-11-01 01:38:55 -0600

    added

--- web/db/account/index.php
+++ web/db/account/index.php
@@ -118,7 +118,7 @@ require_once '../functions.php';
             $email = filter_input(INPUT_POST, 'email');
             $email = checkEmail($email);
             $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
-            $password = checkPassword($password);
+            // $password = checkPassword($password);
     
     // Run basic checks, return if errors
             if (empty($email) || empty($password)) {
