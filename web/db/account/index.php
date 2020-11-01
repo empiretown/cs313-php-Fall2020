448e4604e63b7800ff07c634c72303989d73427f
gracekalu <empiretown2016@gmail.com>
2020-10-31 20:39:52 -0600

    added

--- web/db/account/index.php
+++ web/db/account/index.php
@@ -15,7 +15,7 @@ require_once '../functions.php';
  
  switch ($action) {
      case 'login':
-        header("location: ../view/login.php");
+        header("location: /view/login.php");
      break;
 
      case 'register':
