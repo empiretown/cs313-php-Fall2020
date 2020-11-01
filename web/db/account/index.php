3bcca4f03104704db02cddcd647e0c0849abe3b9
gracekalu <empiretown2016@gmail.com>
2020-10-31 21:49:40 -0600

    added

--- web/db/account/index.php
+++ web/db/account/index.php
@@ -15,7 +15,7 @@ require_once '../functions.php';
  
  switch ($action) {
      case 'login':
-        header("location: /view/login.php");
+        header("location: /db/view/login.php");
      break;
 
      case 'register':
@@ -88,7 +88,7 @@ require_once '../functions.php';
          // Check for existing email address in the table
          if ($checkExistingEmail) {
              $message = '<p class="notice">That email address already exists. Do you want to login instead?</p>';
-             include '../view/login.php';
+             include '../db/view/login.php';
              exit;
          }
  
@@ -102,15 +102,15 @@ require_once '../functions.php';
          // Hash the checked password
          $password = password_hash($password, PASSWORD_DEFAULT);
  
-        //  // Send the data to the model
-        //  $regOutcome = regVisitor($fullname, $email, $username, $password, $phonenumber);
-        //  // Check and report the result --- COOKIES ----
-        //  if ($regOutcome === 1) {
-        //      setcookie('firstname', $firstname, strtotime('+1 year'), '/');
-        //      $message = "<p>Thanks for registering $firstname. Please use your email and password to login.</p>";
-        //      include 'view/login.php';
-        //      exit;
-        //  }
+         // Send the data to the model
+         $regOutcome = regVisitor($fullname, $email, $username, $password, $phonenumber);
+         // Check and report the result --- COOKIES ----
+         if ($regOutcome === 1) {
+             setcookie('firstname', $firstname, strtotime('+1 year'), '/');
+             $message = "<p>Thanks for registering $firstname. Please use your email and password to login.</p>";
+             include '../db/view/login.php';
+             exit;
+         }
          break;
  
      case 'login':
@@ -122,7 +122,7 @@ require_once '../functions.php';
  // Run basic checks, return if errors
          if (empty($email) || empty($passwordCheck)) {
              $message = '<p class="notice">Please provide a valid email address and password.</p>';
-             include '/view/login.php';
+             include '/db/view/login.php';
              exit;
          }
  
@@ -137,7 +137,7 @@ require_once '../functions.php';
  // and return to the login view
          if (!$hashCheck) {
              $message = '<p class="notice">Please check your password and try again.</p>';
-             include '../view/login.php';
+             include '../db/view/login.php';
              exit;
          }
  //A valid user exists, log them in
