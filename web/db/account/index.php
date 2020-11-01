281f66d4a5274287b1ecd68cfec2d7f2c6d823b8
gracekalu <empiretown2016@gmail.com>
2020-10-31 20:06:49 -0600

    added

--- web/db/account/index.php
+++ web/db/account/index.php
@@ -120,7 +120,7 @@ require_once '../functions.php';
  // Run basic checks, return if errors
          if (empty($email) || empty($passwordCheck)) {
              $message = '<p class="notice">Please provide a valid email address and password.</p>';
-             include '/view/login.php';
+             include '/view/registration.php';
              exit;
          }
  
