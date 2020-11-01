70da58b3f02d7519d610c816f0d1517c2398a10d
gracekalu <empiretown2016@gmail.com>
2020-10-31 20:54:52 -0600

    added

--- web/db/account/index.php
+++ web/db/account/index.php
@@ -15,11 +15,11 @@ require_once '../functions.php';
  
  switch ($action) {
      case 'login':
-        header("location: ../view/login.php");
+        header("Location: ../view/login.php");
      break;
 
      case 'register':
-        header("location: ../view/registration.php");
+        header("Location: ../view/registration.php");
      break;
 
      
@@ -122,7 +122,7 @@ require_once '../functions.php';
  // Run basic checks, return if errors
          if (empty($email) || empty($passwordCheck)) {
              $message = '<p class="notice">Please provide a valid email address and password.</p>';
-             include '/view/login.php';
+             include '../view/login.php';
              exit;
          }
  
@@ -137,7 +137,7 @@ require_once '../functions.php';
  // and return to the login view
          if (!$hashCheck) {
              $message = '<p class="notice">Please check your password and try again.</p>';
-             include '/view/login.php';
+             include '../view/login.php';
              exit;
          }
  //A valid user exists, log them in
@@ -156,7 +156,7 @@ require_once '../functions.php';
  
      case 'logout':
          session_destroy();
-         header('Location: ../index.php');
+         header("Location: ../index.php");
          exit;
 }
 ?>
