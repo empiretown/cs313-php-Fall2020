662900242d4c13b6194dbca0c2d40bd5f9797b4f
gracekalu <empiretown2016@gmail.com>
2020-10-31 22:50:28 -0600

    update

--- web/db/account/index.php
+++ web/db/account/index.php
@@ -18,7 +18,7 @@ require_once '../functions.php';
  switch ($action) {
      case 'login':
 
-        include '../view/login.php';
+        //include '../view/login.php';
      break;
 
      case 'register':
