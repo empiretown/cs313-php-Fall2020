802e0854b583fe8c6d975899a41c2005f3ddb1b4
gracekalu <empiretown2016@gmail.com>
2020-10-31 22:33:15 -0600

    update account/index.php

--- web/db/account/index.php
+++ web/db/account/index.php
@@ -14,13 +14,7 @@ require_once '../functions.php';
  }
  
  switch ($action) {
-     case 'login':
-        header("location: ../view/login.php");
-     break;
-
-     case 'register':
-        header("location: ../view/registration.php");
-     break;
+     
 
      
  
