f5246f24a651fc7904f707cfe2508f666d8f38d8
gracekalu <empiretown2016@gmail.com>
2020-10-31 21:22:57 -0600

    added

--- web/db/account/index.php
+++ web/db/account/index.php
@@ -112,6 +112,8 @@ require_once '../functions.php';
             if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
             
                 echo "Welcome to the member's area, " . $_SESSION['username'] . "!";
+                include '../view/login.php';
+
             } else {
                 echo "Please log in first to see this page.";
             }
