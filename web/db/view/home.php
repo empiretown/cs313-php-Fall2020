1fadb17d7fa47e11a531abeacd8f80795f312cf6
gracekalu <empiretown2016@gmail.com>
2020-11-02 13:35:04 -0700

    added

--- web/db/view/home.php
+++ web/db/view/home.php
@@ -10,7 +10,7 @@
   </head>
 
   <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light mr-auto ">
   <img class="navbar-brand"  style="height:80; min-height:80px"src="../images/logo.jpg"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
