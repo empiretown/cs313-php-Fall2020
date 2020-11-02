da70fa5a4e48f1c6e8004ba606e687a263319e6d
gracekalu <empiretown2016@gmail.com>
2020-11-02 13:33:49 -0700

    added

--- web/db/view/home.php
+++ web/db/view/home.php
@@ -16,8 +16,8 @@
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse ml-auto" id="navbarSupportedContent">
-    <ul class="navbar-nav ">
+  <div class="collapse navbar-collapse " id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto ">
       <li class="nav-item active">
         <a class="nav-link" href="../view/home.php">Home <span class="sr-only">(current)</span></a>
       </li>
@@ -39,10 +39,6 @@
         <a class="nav-link disabled" href="#">Disabled</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
 </nav>
    
