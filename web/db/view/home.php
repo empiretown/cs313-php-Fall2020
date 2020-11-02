4e2404cdbee6e503ba174bb7e1f75101e7ac9bce
gracekalu <empiretown2016@gmail.com>
2020-11-02 13:03:27 -0700

    added

--- web/db/view/home.php
+++ web/db/view/home.php
@@ -11,7 +11,7 @@
 
   <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="../images/shed.png">Navbar</a>
+  <img class="navbar-brand" src="../images/shed.png"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
