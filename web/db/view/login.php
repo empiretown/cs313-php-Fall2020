6bc58de2a7eef891f7ac72981dd1b8f1b48c681f
gracekalu <empiretown2016@gmail.com>
2020-11-02 13:24:35 -0700

    added

--- web/db/view/login.php
+++ web/db/view/login.php
@@ -9,16 +9,41 @@
   </head>
 
   <body>
-    <div class="wrapper">
-        <div id="top-header">
-            <a class="logo" href="../../db/view/home.php"><img  class="large-logo" src="../../db/images/shed.png" alt=""></a>
-            <div class="small-logo">
-                    <img src="../../db/images/loginlogo.png" alt="" />
-                <span><a href="../../db/view/login.php">Login</a></span>
-                <img src="../../db/images/cart.png" alt=""><span>Cart</span>
-            </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <img class="navbar-brand"  style="height:80; min-height:80px"src="../images/logo.jpg"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="../view/home.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../view/login.php">Login</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
         </div>
-    </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
     <header class="header">
         <nav class="header-nav" role="navigation">
             <div class="header-nav-brand"></div>
