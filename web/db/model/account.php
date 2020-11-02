b8f237451ec62f91b64798994698f454f29178c9
gracekalu <empiretown2016@gmail.com>
2020-11-02 09:34:53 -0700

    added

--- web/db/model/account.php
+++ web/db/model/account.php
@@ -131,7 +131,7 @@ function getClient($clientEmail) {
 
     $sql = 'SELECT id,email, username, password  FROM customer WHERE email = :clientEmail';
     $stmt = $db->prepare($sql);
-    $stmt->bindValue(':clientEmail', $email, PDO::PARAM_STR);
+    $stmt->bindValue(':clientEmail', $clientEmail, PDO::PARAM_STR);
     
 
     $stmt->execute();
