b5a2d990243cfd3059cafee6dc22ca5014b29e24
gracekalu <empiretown2016@gmail.com>
2020-11-02 13:51:17 -0700

    added

--- web/db/view/login.php
+++ web/db/view/login.php
@@ -61,7 +61,7 @@
 
         <form action="../account/index.php" method="post">
         <h1>SHED MARKET</h1>
-        <div class="form-signin">
+        <div class="container  bg-light ">
              Email Address<br>
             <input type="text" name="clientEmail" id="email" <?php if(isset($clientEmail)){echo "value='$clientEmail'" ;} ?> /><br>
             Password<br>
@@ -80,11 +80,7 @@
             <input type="hidden" name="" value="registration">
         </form>
      </div>
-            Email Address<br>
-            <input type="text" name="clientEmail" id="email" <?php if(isset($clientEmail)){echo "value='$clientEmail'" ;} ?> /><br>
-            Password<br><b><span class="spancss">Password must be at least 8 characters and contain at least 1 number, 1 capital letter and 1 special character</span></b><br>
-            <input type="password" name="clientPassword" id="password" value="" required pattern="(?=^.{8,}$)(?=.*\d)(?=.*\W+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"><br>
-            
+        
 
        
     </main>
