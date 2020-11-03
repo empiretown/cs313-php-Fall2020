f92abec353961da4bfd489a7805157275275d4d6
gracekalu <empiretown2016@gmail.com>
2020-11-03 13:05:00 -0700

    added

--- web/db/view/registration.php
+++ web/db/view/registration.php
@@ -1,26 +1,47 @@
 <!DOCTYPE html>
 <html>
-
-<head>
-    <title>SHED MARKET | Registration</title>
+  <head>
     <meta charset="UTF-8">
+    <title>Shed Market | Login</title>
     <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link href='https://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
     <link href="../../db/css/style.css" rel="stylesheet" type="text/css">
+  </head>
 
-</head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light mr-auto">
+  <img class="navbar-brand"  style="height:80; min-height:80px"src="../images/logo.jpg"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-<body>
-<div class="wrapper">
-        <div id="top-header">
-            <a class="logo" href="../../db/view/home.php"><img  class="large-logo" src="../../db/images/shed.png" alt=""></a>
-            <div class="small-logo">
-                    <img src="../../db/images/loginlogo.png" alt="" />
-                <span><a href="../../db/view/login.php">Login</a></span>
-                <img src="../../db/images/cart.png" alt=""><span>Cart</span>
-            </div>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="../view/home.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../view/login.php">Login</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Category
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
         </div>
-    </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../view/new-prod.php">Add Product</a>
+      </li>
+    </ul>
+    
+  </div>
+</nav>
     <header class="header">
         <nav class="header-nav" role="navigation">
             <div class="header-nav-brand"></div>
@@ -30,32 +51,39 @@
 
         </nav>
     </header>
-        <main>
 
-            <?php
-            if (isset($message)) {
+    <main>
+        <?php
+            if(isset($message)) {
                 echo $message;
             }
-            ?>
-        <form action="../account/index.php" method="post" >
+        ?>
+
+<form action="../account/index.php" method="post" >
             <h1>SHED MARKET</h1>
             Email Address<br>
             <input type="email" name="clientEmail" id="email" <?php if(isset($clientEmail)){echo "value='$clientEmail'" ;} ?> /><br>
             Password<br><b><span class="spancss">Password must be at least 8 characters and contain at least 1 number, 1 capital letter and 1 special character</span></b><br>
             <input type="password" name="clientPassword" id="password" value="" required pattern="(?=^.{8,}$)(?=.*\d)(?=.*\W+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"><br>
-            <inputname="submit" value="Register">
-            <input type="hidden" name="action" value="registration"><br>
+            <input class="button" type="submit" name="submit" value="Sign Up">
+            <input  type="hidden" name="submit" value="registration">
+
+     </div>
+        
         </form>
-        </main>
-    </div>
+       
+    </main>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+</html> 
+    </section>
+
     <footer>
         <hr>
-        <br />
-        <p>2020<p>
-                <br />
+        <br>
+        <p>&copy; 2020 SHED MARKET</p>
     </footer>
-    <script src="../js/car.js">
-    </script>
-</body>
 
+  </body>
 </html>
