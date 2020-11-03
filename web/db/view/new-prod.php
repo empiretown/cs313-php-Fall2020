b333c4c465218718a97146b2b9b4f8b0a3b96a78
gracekalu <empiretown2016@gmail.com>
2020-11-03 10:27:02 -0700

    added

--- web/db/view/new-prod.php
+++ web/db/view/new-prod.php
@@ -8,7 +8,7 @@ if ($_SESSION['clientData']['clientLevel'] == 1) {
 <html>
 
 <head>
-    <title>SHED MARKET | Login</title>
+    <title>SHED MARKET | Create a new product</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
     <link href='https://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
@@ -17,16 +17,38 @@ if ($_SESSION['clientData']['clientLevel'] == 1) {
 </head>
 
 <body>
-<div class="wrapper">
-        <div id="top-header">
-            <a class="logo" href="../../db/view/home.php"><img  class="large-logo" src="../../db/images/shed.png" alt=""></a>
-            <div class="small-logo">
-                    <img src="../../db/images/loginlogo.png" alt="" />
-                <span><a href="?action=login.php">Login</a></span>
-                <img src="../../db/images/cart.png" alt=""><span>Cart</span>
-            </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light mr-auto">
+  <img class="navbar-brand"  style="height:80; min-height:80px"src="../images/logo.jpg"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="../view/home.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../view/login.php">Login</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
         </div>
-    </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    
+  </div>
+</nav>
     <header class="header">
         <nav class="header-nav" role="navigation">
             <div class="header-nav-brand"></div>
