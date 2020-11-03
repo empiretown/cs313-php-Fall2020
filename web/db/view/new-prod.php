675d349417e77e7adbb345334c7e867c59b0a9f8
gracekalu <empiretown2016@gmail.com>
2020-11-03 10:32:57 -0700

    added

--- web/db/view/new-prod.php
+++ web/db/view/new-prod.php
@@ -12,6 +12,8 @@ if ($_SESSION['clientData']['clientLevel'] == 1) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
     <link href='https://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
     <link href="../../db/css/style.css" rel="stylesheet" type="text/css">
 
 </head>
@@ -109,7 +111,8 @@ if ($_SESSION['clientData']['clientLevel'] == 1) {
         <p>&copy;  2020 SHED MARKET<p>
         <br />
     </footer>
-    <script src="../js/car.js">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     </script>
 </body>
 
