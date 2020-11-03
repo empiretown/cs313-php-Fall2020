a92fda0a40db934a5be7aa8cf988aeca12c172b3
gracekalu <empiretown2016@gmail.com>
2020-11-03 13:15:01 -0700

    added

--- web/db/view/category.php
+++ web/db/view/category.php
@@ -2,9 +2,10 @@
 <html>
 
 <head>
-    <title>SHEDMARKET | Category</title>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
+    <title>Shed Market</title>
     <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link href='https://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
     <link href="../../db/css/style.css" rel="stylesheet" type="text/css">
 
