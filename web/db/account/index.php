04136547a7266775f0f2c289b09ea2f2dc404943
gracekalu <empiretown2016@gmail.com>
2020-11-03 12:56:59 -0700

    added

--- web/db/account/index.php
+++ web/db/account/index.php
@@ -27,34 +27,34 @@ require_once '../functions.php';
       case 'registration':
 
         echo ("i registered");
+        $clientEmail = filter_input(INPUT_POST, 'clientEmail', FILTER_SANITIZE_STRING);
+    $clientPassword = filter_input(INPUT_POST, 'clientPassword', FILTER_SANITIZE_STRING);
     
-      //   $clientEmail = filter_input(INPUT_POST, 'clientEmail', FILTER_SANITIZE_EMAIL);
-      //   $clientPassword = filter_input(INPUT_POST, 'clientPassword', FILTER_SANITIZE_STRING);
-        
-      //   $checkPassword = checkPassword($clientPassword);
+    $checkPassword = checkPassword($clientPassword);
+
+    if (empty($clientEmail) || empty($checkPassword)) {
+      $_SESSION['message'] = '<p>Please provide information for all empty form fields.</p>';
+      header("Location: ../view/registration.php");
+      exit;
+    }
+
+    $hashedPassword = password_hash($clientPassword, PASSWORD_DEFAULT);
+
+    $regOutcome = registerClient($clientEmail, $hashedPassword);
+
+    if($regOutcome === 1){
+      setcookie('email', $clientEmail, strtotime('+1 year'), '/');
+      $_SESSION['message'] = "<p>Thanks for registering, $clientEmail. Please use your email and password to login.</p>";
+      header("Location: /week7ta/login.php");
+      exit;
+     } else {
+      $_SESSION['message'] = "<p>Sorry $clientEmail, but the registration failed. Please try again.</p>";
+      header("Location: /week7ta/registration.php");
+      exit;
+     }
     
-      //   if (empty($clientEmail) || empty($checkPassword)) {
-      //     $_SESSION['message'] = '<p>Please provide information for all empty form fields.</p>';
-      //     header("Location: ../view/registration.php");
-      //     exit;
-      //   }
     
-      //   $hashedPassword = password_hash($clientPassword, PASSWORD_DEFAULT);
-    
-      //   $regOutcome = registerClient($clientEmail, $hashedPassword);
-    
-      //   if($regOutcome === 1){
-      //     setcookie('firstname', $clientEmail, strtotime('+1 year'), '/');
-      //     $_SESSION['message'] = "<p>Thanks for registering, $clientEmail. Please use your email and password to login.</p>";
-      //     header("Location: ../view/login.php");
-      //     exit;
-      //    } else {
-      //     $_SESSION['message'] = "<p>Sorry $clientEmail, but the registration failed. Please try again.</p>";
-      //     header("Location: ../view/registration.php");
-      //     exit;
-      //    }
-    
-      //   break;
+    break;
     
       // case 'Logging':
     
